4603965e506668a05ca3d2146380f7990bbc7b6f
Johnathan238 <jonathanumana86@gmail.com>
2022-08-05 01:46:02 -0400

    Used ngFor to loop Ingredients & used string interpilation to render to DOM

--- Recipe-App/src/app/shopping-list/shopping-list.component.ts
+++ Recipe-App/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredients-modal';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients = []
+  ingredients: Ingredient[] = [
+    new Ingredient(1, 'Kosher salt'),
+    new Ingredient(1, 'pound spaghetti')
+  ]
   constructor() { }
 
   ngOnInit(): void {
