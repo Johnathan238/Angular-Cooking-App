857155f857974b0a10e324b1f4f19211279b0c27
Johnathan <jonathanumana86@gmail.com>
2022-08-31 01:15:04 -0400

    Starting using local references

--- Recipe-App/src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ Recipe-App/src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-list-edit',
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list-edit.component.css']
 })
 export class ShoppingListEditComponent implements OnInit {
-
+  @ViewChild('nameInput') nameInputRef: ElementRef
+  @ViewChild('amountInput') amountInputRef: ElementRef
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onAddItem(){
+
   }
 
 }
